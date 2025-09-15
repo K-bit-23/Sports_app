1da999d40ef6ae56e07655bd667dd5f2ff67934a
jothisankar979 <jothisankar979@gmail.com>
2025-09-15 09:01:52 +0000

    dl

--- sports-app-react-native/app/(tabs)/_layout.tsx
+++ sports-app-react-native/app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+
 import { Tabs } from 'expo-router';
 import React from 'react';
 
@@ -28,6 +29,13 @@ export default function TabLayout() {
         options={{
           title: 'Explore',
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="dashboard"
+        options={{
+          title: 'Dashboard',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="square.grid.2x2.fill" color={color} />,
         }}
       />
     </Tabs>
