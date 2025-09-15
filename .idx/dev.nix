1154f1c0687717e1c494248e7ea590b12adfe064
karthikeyanp.24mca <karthikeyanp.24mca@kongu.edu>
2025-09-15 09:50:09 +0000

    d

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -6,7 +6,7 @@
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
-    pkgs.nodejs_20
+    pkgs.nodejs
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
